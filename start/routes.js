f1ba1250468068d1526a08430248990af3f1d387
iqbal <emixbal@gmail.com>
2019-05-26 17:53:32 +0700

    routing

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+// Route.on('/').render('welcome')
+
+
+Route.post('/register','UserController.register');
