94dc16ee472edd6428e2d90385650b2ea58eceb6
iqbal <emixbal@gmail.com>
2019-05-26 17:50:13 +0700

    create hook to auto generate UUID

--- app/Models/User.js
+++ app/Models/User.js
@@ -5,9 +5,20 @@ const Hash = use('Hash')
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const uuid = require('uuid');
 
 class User extends Model {
+  static get hidden () {
+    return ['password']
+  }
+  // static get primaryKey () {
+  //   return 'id'
+  // }
+  
   static boot () {
+    const user = {
+      id : uuid.v4()
+    }
     super.boot()
 
     /**
@@ -18,6 +29,14 @@ class User extends Model {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
       }
+    })
+    this.addHook('beforeCreate', async (userInstance) => {
+      userInstance.id = user.id
+    })
+    this.addHook('afterCreate', async (userInstance) => {
+      userInstance.id = await user.id
+      await delete user.id
+      user.id = await uuid.v4()
     })
   }
 
