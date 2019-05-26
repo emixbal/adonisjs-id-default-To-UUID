04c72fb2dc6ca422a53bd9594d4fb6b88bd4a26a
iqbal <emixbal@gmail.com>
2019-05-26 16:11:42 +0700

    define user schema

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -6,11 +6,17 @@ const Schema = use('Schema')
 class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
-      table.increments()
+      table.uuid('id').primary()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
-      table.timestamps()
+      table.string('verification_token')
+      table.boolean('is_verified').defaultTo(true)
+      table.date('birthday').notNullable()
+      table.string('phone_number', 14).notNullable()
+      table.text('address').notNullable()
+      table.timestamp('created_at').defaultTo(this.fn.now())
+      table.timestamp('updated_at').defaultTo(this.fn.now())
     })
   }
 
